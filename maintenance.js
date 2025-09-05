a015edff9c21e96224ba1b65865f050016c17955
Sami.s <Samisingh988@gmail.com>
2025-09-05 15:29:27 +1000

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -1,6 +1,6 @@
 // ======= CONFIG =======
 const maintenance = {
-  active: false,             // true = maintenance on, false = off
+  active: true,             // true = maintenance on, false = off
   endTime: "2025-09-06T12:00:00", // maintenance end time
   page: "maintenance.html"  // page to show
 };
